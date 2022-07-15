9824d3c8850f3f1144e28cded10b3c979fe4a90c
armanharut98506 <armanharut7117@gmail.com>
2022-07-15 21:40:44 +0400

    Solution for task10

--- cmd/muxing/muxing.go
+++ cmd/muxing/muxing.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"io/ioutil"
 
 	"github.com/gorilla/mux"
 )
@@ -17,9 +18,52 @@ Feel free to drop gorilla.mux if you want and use any other solution available.
 main function reads host/port from env just for an example, flavor it following your taste
 */
 
+func NameHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Fprintf(w, "Hello, %s!", vars["param"])
+}
+
+func BadHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+}
+
+func DataHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Fprintf(w, "I got message:\n%s", string(body))
+	}
+}
+
+func HeadersHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		a := r.Header.Get("a")
+		b := r.Header.Get("b")
+
+		a2, err := strconv.Atoi(a)
+		if err != nil {
+			panic(err)
+		}
+		b2, err := strconv.Atoi(b)
+		if err != nil {
+			panic(err)
+		}
+		ab := strconv.Itoa(a2 + b2)
+
+		w.Header().Set("a+b", ab)
+	}
+}
+
 // Start /** Starts the web server listener on given host and port.
 func Start(host string, port int) {
 	router := mux.NewRouter()
+
+	router.HandleFunc("/name/{param}", NameHandler)
+	router.HandleFunc("/bad", BadHandler)
+	router.HandleFunc("/data", DataHandler)
+	router.HandleFunc("/headers", HeadersHandler)
 
 	log.Println(fmt.Printf("Starting API server on %s:%d\n", host, port))
 	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), router); err != nil {
